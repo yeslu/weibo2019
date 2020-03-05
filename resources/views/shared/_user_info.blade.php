6371ca49b216b14c5a0e60514febbba568bb4936
vagrant <vagrant@homestead>
2020-03-05 09:35:17 +0000

    创建会话

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,5 +1,4 @@
 <a href="{{ route('users.show', $user->id) }}">
   <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
-  {{ $user->gravatar('140') }}
 </a>
 <h1>{{ $user->name }}</h1>
