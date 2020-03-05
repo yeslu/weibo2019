82e2ef1cf8e1387eb81e88aa0b9b38792ff42bc5
vagrant <vagrant@homestead>
2020-03-05 09:52:24 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -22,7 +22,7 @@ class SessionsController extends Controller
 
        $email=$request->email;
        $password=$request->password;
-        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
            return redirect()->route('users.show', [Auth::user()]);
         } else {
