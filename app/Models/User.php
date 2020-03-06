27eb3c075d072ec613fc6529e5378c3a7c207c82
vagrant <vagrant@homestead>
2020-03-06 09:37:53 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,35 @@ class User extends Authenticatable
         return $this->statuses()
                     ->orderBy('created_at', 'desc');
     }
+    //获取粉丝关系列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+    //关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+    //判断当前登录的用户 A 是否关注了用户 B
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
