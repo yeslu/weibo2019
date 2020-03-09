9141d1e50787fe704782239d6bb446779571404a
vagrant <vagrant@homestead>
2020-03-09 02:41:15 +0000

    修改主页

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -22,9 +22,9 @@
     </div>
   @else
     <div class="jumbotron">
-      <h1>Hello Laravel</h1>
+      <h1>主页</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+        进入微博
       </p>
       <p>
         一切，将从这里开始。
